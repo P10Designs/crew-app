f5e4e4901179a024d135c26a653555538d1484f6
Pedro Soba <designs.p10@gmail.com>
2022-03-28 17:00:45 +0200

    se

---
+++ firebase/clientApp.ts
@@ -0,0 +1,34 @@
+import { getApp, getApps, initializeApp } from 'firebase/app'
+
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  connectFirestoreEmulator
+} from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCAoN9pJ9IjGNYtOXmc9KKNiGBlDlU9X8k',
+  authDomain: 'crew-eb850.firebaseapp.com',
+  projectId: 'crew-eb850',
+  storageBucket: 'crew-eb850.appspot.com',
+  messagingSenderId: '356215413236',
+  appId: '1:356215413236:web:03fa759be6850e260581a1'
+}
+
+if (!getApps.length) {
+  initializeApp(firebaseConfig)
+}
+export const db = getFirestore()
+
+if (!getApp.length) {
+  connectFirestoreEmulator(db, 'localhost', 8080)
+}
+
+export const getEvents = async () => {
+  const eventsCol = collection(db, 'events')
+  const eventSnap = await getDocs(eventsCol)
+  const eventList = eventSnap.docs.map(doc => doc.data())
+  console.log(eventList)
+  return eventList
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,37 @@
-import { NextPage } from 'next'
+import { NextPage, GetServerSideProps } from 'next'
 import { useState, useEffect } from 'react'
 import 'dayjs/locale/es'
 import dayjs from 'dayjs'
 import { Calendar } from '@mantine/dates'
-
-interface eventInterface {
-  day: Number,
-  month: Number,
-  year: Number
+import { getEvents } from '../firebase/clientApp'
+interface eventMenuInterface {
+  date: {
+    seconds: number,
+    nanoseconds: number
+  },
+  positions: [
+    {
+      name: string,
+      special: boolean,
+      assigned: string
+    }
+  ]
 }
 
-const Home: NextPage = () => {
+interface HomeProps{
+  events: string
+}
+
+const Home: NextPage<HomeProps> = (props) => {
   const [month, setMonth] = useState(new Date())
   const [thisMonthEvents, setThisMonthEvents] = useState<[Number]>()
-  const [events, setEvents] = useState<eventInterface[]>()
+  const [events, setEvents] = useState<eventMenuInterface[]>()
+  const [menu, setMenu] = useState('calendar')
+  const [selected, setSelected] = useState(new Date())
 
   useEffect(() => {
     if (events === undefined) {
-      setEvents([
-        { day: 10, month: 1, year: 2022 },
-        { day: 12, month: 4, year: 2022 },
-        { day: 28, month: 3, year: 2022 }
-      ])
+      setEvents(JSON.parse(props.events))
     }
     if (events !== undefined) handleMonthEvents()
   }, [events, month])
@@ -31,59 +41,87 @@ const Home: NextPage = () => {
     if (events !== undefined) {
       for (let i = 0; i < events.length; i++) {
         const event = events[i]
-        if (event.month === month.getMonth() + 1 && event.year === month.getFullYear()) array.push(event.day)
+        const date = new Date(event.date.seconds * 1000)
+        if (date.getMonth() === month.getMonth() && date.getFullYear() === month.getFullYear()) array.push(date.getDate())
       }
     }
     setThisMonthEvents(array)
   }
 
-  const onMonthHandle = (e:Date) => {
-    setMonth(e)
-  }
   const dayClick = (e:Date) => {
     if (thisMonthEvents?.includes(e.getDate())) {
-      console.log('yes')
+      setSelected(e)
+      setMenu('dayList')
     }
   }
   return (
-        <div className='w-full h-full flex flex-col items-center justify-center bg-gray-500'>
-          <div className='w-5/6 lg:w-1/2 '>
-            <Calendar
-              onChange={(e) => { dayClick(e) }}
-              month={month}
-              onMonthChange={(e) => { onMonthHandle(e) }}
-              fullWidth
-              minDate={ dayjs(new Date()).toDate() }
-              size="xl"
-              locale='es'
-              dayStyle={(date) =>
-                thisMonthEvents?.includes(date.getDate())
-                  ? { backgroundColor: '#dc2626', color: 'white' }
-                  : date.toDateString() === new Date().toDateString()
-                    ? { backgroundColor: '#2563eb', color: 'white' }
-                    : { backgroundColor: '' }
+        <div className='w-full flex flex-col items-center justify-center bg-gray-500 h-min'>
+          <div className='font-bold text-5xl mb-7'>CREW APP</div>
+          {menu === 'calendar' &&
+            <>
+              <div className='w-5/6 lg:w-1/2 '>
+                <Calendar
+                  onChange={(e) => { dayClick(e) }}
+                  month={month}
+                  onMonthChange={setMonth}
+                  fullWidth
+                  minDate={ dayjs(new Date()).toDate() }
+                  size="xl"
+                  locale='es'
+                  dayStyle={(date) =>
+                    thisMonthEvents?.includes(date.getDate())
+                      ? { backgroundColor: '#dc2626', color: 'white' }
+                      : date.toDateString() === new Date().toDateString()
+                        ? { backgroundColor: '#2563eb', color: 'white' }
+                        : { backgroundColor: '' }
+                  }
+                  hideOutsideDates
+                  allowLevelChange={false}
+                  styles={() => ({
+                    cell: { border: '1px solid #adb5bd' },
+                    day: { borderRadius: 0, height: 70, color: 'white', fontWeight: '900' },
+                    weekdayCell: { height: 70 }
+                  })}
+                />
+              </div>
+              <div className='inline-flex'>
+                <div className='flex flex-col items-center justify-center m-5'>
+                  <span className='bg-red-600 text-transparent'>leye</span>
+                  <span >Eventos</span>
+                </div>
+                <div className='flex flex-col items-center justify-center m-5'>
+                  <span className='bg-blue-600 text-transparent'>leye</span>
+                  <span >Hoy</span>
+                </div>
+              </div>
+              <div className='text-xs text-start w-5/6 mt-3'>* Para acceder a los diferentes eventos, haz click en uno de los dias con evento para desplegar las opciones</div>
+            </>
+          }
+          {menu === 'dayList' &&
+            <div className='flex flex-col itemc-center justify-center'>
+              <button onClick={() => { setMenu('calendar') }}>CLOSE</button>
+              {events !== undefined && events.map((event, i) => {
+                if (new Date(event.date.seconds * 1000).getDate() !== selected.getDate()) return (<></>)
+                return (
+                    <div>
+                      {event.date.seconds}
+                    </div>
+                )
+              })
               }
-              hideOutsideDates
-              allowLevelChange={false}
-              styles={() => ({
-                cell: { border: '1px solid #adb5bd' },
-                day: { borderRadius: 0, height: 70, color: 'white', fontWeight: '900' },
-                weekdayCell: { height: 70 }
-              })}
-            />
-          </div>
-          <div className='inline-flex'>
-            <div className='flex flex-col items-center justify-center m-5'>
-              <span className='bg-red-600 text-transparent'>leye</span>
-              <span >Eventos</span>
             </div>
-            <div className='flex flex-col items-center justify-center m-5'>
-              <span className='bg-blue-600 text-transparent'>leye</span>
-              <span >Hoy</span>
-            </div>
-          </div>
+          }
         </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const events = await getEvents()
+  return {
+    props: {
+      events: JSON.stringify(events)
+    }
+  }
 }
 
 export default Home
